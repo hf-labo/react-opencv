9165c9a25afceead770318600b8c186952b235ce
HFラボ <hf.labo.free@gmail.com>
2022-08-17 21:42:50 +0900

    グレイスケール化、二値化機能追加

--- src/App.js
+++ src/App.js
@@ -14,10 +14,28 @@ function App() {
       img.src = URL.createObjectURL(e.target.files[0])
     }
   }
+  
+  const onClickGray = () => {
+    const mat = cv.imread('output')
+    cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY, 0)
+    cv.imshow('output', mat)
+    mat.delete()
+  }
+
+  const onClickTh = () => {
+    const mat = cv.imread('output')
+    if (mat.channels() !== 1) cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY, 0)
+    cv.threshold(mat, mat, 0, 255, cv.THRESH_OTSU)
+    cv.imshow('output', mat)
+    mat.delete()
+  }
+
   return (
     <div className="App">
       <div>
         <input type="file" onChange={onChangeFile} />
+        <button onClick={onClickGray}>グレイスケール化</button>
+        <button onClick={onClickTh}>二値化</button>
       </div>
       <canvas id="output" />
     </div>
